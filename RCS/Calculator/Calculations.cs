0902d8625f8d93b350a5975f3ddf3b7557779218
Reinis Vesers <reinis.vesers@accenture.com>
2017-12-20 20:17:48 +0200

    added calculator project

---
+++ RCS/Calculator/Calculations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculations
+    {
+        public int AskUserForNumber()
+        {
+            // izvadīt tekstu, kas paprasa ciparu
+            Console.WriteLine("please enter number:");
+            // ielasīt ciparu no konsoles
+            // izveido mainīgo, kas glabās tekstu
+            int number;
+            // ieraksta mainīgajā ReadLine funkcijas rezultātu
+            string inputText = Console.ReadLine();
+
+            // pārveido lietotāja tekstu par ciparu
+            bool success = Int32.TryParse(inputText, out number);
+
+            if (success == false)
+            {
+                Console.WriteLine("sorry, wrong value entered");
+                number = AskUserForNumber();
+            }
+
+            return number;
+        }
+    }
+}
---
+++ RCS/Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // izveido kalkulēšanas objektu
+            Calculations calc;
+            calc = new Calculations();
+
+            // paprasīt lietotājam vērtību
+            int firstNumber = calc.AskUserForNumber();
+
+            // paprasīt lietotājam otru vērtību
+            int secondNumber = calc.AskUserForNumber();
+
+            // saskaita
+            int result = firstNumber + secondNumber;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
