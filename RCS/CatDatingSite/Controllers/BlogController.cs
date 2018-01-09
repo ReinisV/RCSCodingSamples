8338e6e0c4ba1dec2dae60e6e79bb000075aeed2
Reinis Vesers <reinis.vesers@accenture.com>
2018-01-09 21:47:14 +0200

    added blogs

--- RCS/CatDatingSite/Controllers/BlogController.cs
+++ RCS/CatDatingSite/Controllers/BlogController.cs
@@ -6,13 +6,28 @@ using System.Web.Mvc;
 
 namespace CatDatingSite.Controllers
 {
+    using System.Data.Entity;
+    using System.Web.Security;
+
     using CatDatingSite.Models;
+
+    using Microsoft.AspNet.Identity;
 
     public class BlogController : Controller
     {
         // GET: Blog
         public ActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                string userId = User.Identity.GetUserId();
+                using (var db2 = new ApplicationDbContext())
+                {
+                    var user = db2.Users.First(u => u.Id == userId);
+                }
+            }
+
+            
             using (var db = new CatDb())
             {
                 var blogsList = db.Blogs.ToList();
@@ -23,6 +38,22 @@ namespace CatDatingSite.Controllers
         public ActionResult Create()
         {
             return View();
+        }
+
+        public ActionResult Edit(Blog model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (var db = new CatDb())
+            {
+                db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
